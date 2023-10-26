0c26c37a4812ea4c4a49efd30b43d1af946f46eb
mariik <m.portnykh@fxdev.sk>
2023-10-26 18:07:15 +0200

    feat: start command_n development; craate ids

--- projekt_1.c
+++ projekt_1.c
@@ -14,6 +14,24 @@ int skontrolovat_subor(FILE* file) {
   return 1;
 }
 
+// Posuvam ukazovatel v subore na zaciatok suboru
+int to_file_start(FILE* file) {
+  if (fseek(file, 0, SEEK_SET) == 0) return 1;
+  return 0;
+}
+
+/*
+vzhladom na to, ze na konci kazdeho riadku
+je nejaky neurcity pocet medzier,
+touto funkciou sa presuvam na dalsi riadok
+*/
+void new_line(FILE** file) {
+  char c;
+  while ((c = getc(*file)) != '\n' && c != EOF) {
+    continue;
+  }
+}
+
 // otvorit subor
 int command_v(FILE** file) {
   *file = fopen("dataloger.txt", "r");
@@ -23,14 +41,51 @@ int command_v(FILE** file) {
   return 1;
 }
 
+int command_n(FILE** file, char*** ids) {
+  int file_status = skontrolovat_subor(*file);
+  if (!file_status) return 0;
+
+  char symbol;
+  int pocet_zoznamov = 0;
+  while ((symbol = getc(*file)) != EOF) {
+    if (symbol == '\n') {
+      pocet_zoznamov++;
+    }
+  }
+  pocet_zoznamov = (pocet_zoznamov + 1) / 7;
+  to_file_start(*file);
+
+  *ids = (char**)malloc(pocet_zoznamov * sizeof(char*));
+
+  int id_size = 6;
+
+  for (int i = 0; i < pocet_zoznamov; i++) {
+    // praca s id
+    (*ids)[i] = (char*)malloc(id_size * sizeof(char));
+    for (int j = 0; j < id_size; j++) {
+      (*ids)[i][j] = getc(*file);
+    }
+
+    new_line(&*file);
+  }
+
+  printf("pocet zaznaov %d\n", pocet_zoznamov);
+}
+
 int main() {
   char command;
   FILE* dataloger_file;
   int v_stav, n_stav;
 
+  char** ids;
+
   do {
     scanf("%c", &command);
     if (command == 'v') v_stav = command_v(&dataloger_file);
+    if (command == 'n') {
+      n_stav = command_n(&dataloger_file, &ids);
+      printf("%s", ids[0]);
+    }
   } while (command != 'k');
 
   fclose(dataloger_file);
